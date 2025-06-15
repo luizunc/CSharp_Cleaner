8b90fa8a0440d26b61ebe680b2329119cf7901bc
luizunc <redgameplay33@gmail.com>
2025-06-15 11:22:38 -0300

    v1

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associada a um assembly.
+[assembly: AssemblyTitle("Cleaner")]
+[assembly: AssemblyDescription("Automatização para Limpezas profundas, sob direitos de eduzp")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Estúdios Unc")]
+[assembly: AssemblyProduct("Cleaner")]
+[assembly: AssemblyCopyright("eduzp | Estúdios Unc")]
+[assembly: AssemblyTrademark("Unc Series")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna invisíveis os tipos neste assembly
+// para componentes COM.  Caso precise acessar um tipo neste assembly a partir de 
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("ea49b719-9ee7-43e0-80aa-72ef9f02f147")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1")]
+[assembly: AssemblyFileVersion("1")]
+[assembly: NeutralResourcesLanguage("pt-BR")]
